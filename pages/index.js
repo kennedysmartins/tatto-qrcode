3ba239b5008f3a82ca35f75b204a17906d830696
Kenny <kenybustamante@hotmail.com>
2021-02-08 15:02:34 -0300

    redirect package

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,2 @@
-import { useEffect} from "react";
-import { useRouter} from "next/router"
-
-
-export default function Home(){
-    const router = useRouter();
-    useEffect(() => {
-        console.log('Redirect')
-        router.push('https://google.com.br')
-    }, [])
-
-}
-
-// componentDidMount(){
-//     const {pathname} = Router
-//     if(pathname == '/' ){
-//        Router.push('https://www.google.com')
-//     }
-// }
-
-// export default componentDidMount
-
-
-
+import redirect from 'nextjs-redirect'
+export default redirect('https://google.com.br')
