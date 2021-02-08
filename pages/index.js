2c20caba5bb647050eaea776b014b78311c83a77
Kenny <kenybustamante@hotmail.com>
2021-02-08 14:43:18 -0300

    new test router

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
-import Router from 'next/router'
-
 import { useEffect, useState} from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function Home(){
-    return (
-        <div>
-            <Link href="https://google.com.br">
-                <a>Google</a>
-            </Link>
-        </div>
-    )
+    const {pathname} = useRouter()
+    if(pathname == '/') {
+        useRouter.push('https://google.com.br')
+    }
 }
 
 // componentDidMount(){
