72dbce27a8156bfb1f93d95fd69adb4da108c80d
Kennedy Martins <kenybustamante2@hotmail.com>
2021-12-06 18:26:07 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,5 @@
 import redirect from 'nextjs-redirect'
-export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/')
+
+
+export default redirect('https://www.youtube.com/watch?v=xI9kjXRfhmo')
+//export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/')
