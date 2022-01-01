2d8ebb06852a2da71823058f1117e5af980cb11d
Kennedy Martins <kenybustamante2@hotmail.com>
2022-01-01 02:40:00 -0300

    novo vídeo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import redirect from 'nextjs-redirect'
 
 
-export default redirect('https://www.youtube.com/watch?v=xI9kjXRfhmo')
+export default redirect('https://youtu.be/tRRMhN3PqbQ')
 //export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/')
