4ab9c94857687735fa90565cad21ee444211e366
Kennedy Martins <kenybustamante2@hotmail.com>
2023-04-17 12:46:31 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import redirect from 'nextjs-redirect'
 //export default redirect('https://youtu.be/x-ruCFtzF08')
 
 
-//export default redirect('https://youtu.be/tRRMhN3PqbQ')
-export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/')
+export default redirect('https://youtu.be/tRRMhN3PqbQ')
+//export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/')
 //export default redirect('https://instagram.com/kenny.mrts?igshid=YmMyMTA2M2Y') 
 //export default redirect('https://pix.tecker.com.br')
