5fbbd234fb5f34b154548d187844d72b8e6fab12
Kennedy Martins <kennedy@tecker.com.br>
2024-02-12 11:33:28 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import redirect from 'nextjs-redirect'
 
 //export default redirect('https://youtu.be/x-ruCFtzF08')
 //export default redirect('https://linktr.ee/kennedysmartins')
-//export default redirect('https://me-contrate.vercel.app/me/kennedysmartins')
+export default redirect('https://me-contrate.vercel.app/me/kennedysmartins')
 //export default redirect('https://corte84.com.br')
 //export default redirect('https://youtu.be/tRRMhN3PqbQ')
-export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/')
+//export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/')
 //export default redirect('https://instagram.com/kenny.mrts?igshid=YmMyMTA2M2Y') 
 //export default redirect('https://pix.tecker.com.br')
