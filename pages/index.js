c915782d1aa6ab151f4219e356baec9a7381e2ad
Kennedy Martins <kennedy@tecker.com.br>
2024-10-29 09:16:27 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,4 +9,6 @@ import redirect from 'nextjs-redirect'
 //export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/')
 //export default redirect('https://instagram.com/kenny.mrts?igshid=YmMyMTA2M2Y') 
 //export default redirect('https://pix.tecker.com.br')
-export default redirect ('https://kennedymartins.dev')
+//export default redirect ('https://kennedymartins.dev')
+export default redirect ('http://upm66w.online/get.php?username=tesssst&password=T5nS5g&type=m3u_plus&output=ts')
+
