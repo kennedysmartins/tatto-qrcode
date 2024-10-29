18a035ad1d361d5ba5d3fc24219f73df2d6fa76b
Kennedy Martins <kennedy@tecker.com.br>
2024-10-29 10:07:17 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import redirect from 'nextjs-redirect'
 
-//export default redirect('https://youtu.be/xI9kjXRfhmo?si=EzJWFJSuoajjKxHs') 
-//export default redirect('https://youtu.be/x-ruCFtzF08')
+//export default redirect('https://youtu.be/xI9kjXRfhmo?si=EzJWFJSuoajjKxHs')  //Bebado cantando inglês
+export default redirect('https://youtu.be/x-ruCFtzF08') // Peixe
 //export default redirect('https://linktr.ee/kennedysmartins')
 //export default redirect('https://me-contrate.vercel.app/me/kennedysmartins')
 //export default redirect('https://corte84.com.br')
-//export default redirect('https://youtu.be/tRRMhN3PqbQ')
-//export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/')
-//export default redirect('https://instagram.com/kenny.mrts?igshid=YmMyMTA2M2Y') 
+//export default redirect('https://youtu.be/tRRMhN3PqbQ') //Zé da Timba e Zé Latinha
+//export default redirect('https://portal.worldcast.io/cast/BjWQVwOXKbZon4xNQXVO8A73qGdPmy2a5Lkl/') //Shrek
+//export default redirect('https://instagram.com/kenny.mrts') 
 //export default redirect('https://pix.tecker.com.br')
 //export default redirect ('https://kennedymartins.dev')
-export default redirect ('http://upm66w.online/get.php?username=tesssst&password=T5nS5g&type=m3u_plus&output=ts')
 
